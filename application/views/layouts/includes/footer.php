0cc47e92e7cb54ecc6d83f071dd0fca9ef79ba67
ShaheryarK <shaheryar.khan090078601@gmail.com>
2019-04-10 22:50:19 +0500

    first commit

---
+++ application/views/layouts/includes/footer.php
@@ -0,0 +1,22 @@
+
+				 </div>
+			 </div>
+	    </div>
+    </div>
+	<br>
+	<br>
+	<br>
+  </div><!-- /.container -->
+
+	  <div class=" row footer">
+	    <div class="container">
+	       <p>Reality Studio &copy; 2017, All Rights Reserved</p>
+	    </div>
+	  </div>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    
+  </body>
+</html>
---
+++ application/views/thank.php
@@ -0,0 +1,4 @@
+<P><strong>The payment method will be soon implemented ThankYou!! for testing my Website :)
+       <br>
+<br>	   Name of Developer: STAK   "aka Bse153096" 
+</strong></p>
